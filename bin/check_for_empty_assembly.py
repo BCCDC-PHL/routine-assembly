5f6f765f5d916737573058d96e988f2e1fb7cf38
Dan Fornika <dfornika@gmail.com>
2023-08-29 12:04:51 -0700

    Fix duplicated sample ID

--- bin/check_for_empty_assembly.py
+++ bin/check_for_empty_assembly.py
@@ -7,11 +7,10 @@ def main(args):
     input_num_bytes = os.path.getsize(args.assembly)
     if input_num_bytes == 0:
         with open(args.assembly, 'w') as f:
-            f.write('>' + args.sample_id + '_1\nN\n')
+            f.write('>1\nN\n')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--assembly', help='Input assembly to check', required=True)
-    parser.add_argument('--sample-id', help='Sample ID to write to header if empty assembly is found', required=True)
     args = parser.parse_args()
     main(args)
