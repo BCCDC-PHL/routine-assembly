8d6397561c728712d0720b4e969fa00343ce2c1c
Dan Fornika <dfornika@gmail.com>
2024-04-23 15:55:14 -0700

    Add workflow and starter scripts

---
+++ .github/scripts/build_container_images.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for env_yaml in ./environments/*.yml; do
+   echo "building $env_yaml..."
+done
---
+++ .github/scripts/install_apptainer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo pipefail
+
+wget https://raw.githubusercontent.com/apptainer/apptainer/main/tools/install-unprivileged.sh
+
+chmod +x install-unprivileged.sh
+
+mkdir -p /opt/apptainer
+
+./install-unprivileged.sh /opt/apptainer
