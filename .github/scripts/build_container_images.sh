c73d8bc2d8ca7e51b3fe5ed8e0778b5a623c6185
Dan Fornika <dfornika@gmail.com>
2024-04-23 16:04:52 -0700

    Preparing to build images

--- .github/scripts/build_container_images.sh
+++ .github/scripts/build_container_images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 for env_yaml in ./environments/*.yml; do
-   echo "building $env_yaml..."
+    image_name=$(head -n 1 $env_yaml | cut -d ' ' -f 2)
+    echo "building image ${image_name} from file ${env_yaml}..."
 done
