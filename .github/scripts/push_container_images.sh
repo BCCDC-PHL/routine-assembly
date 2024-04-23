0a8151a3f3dc38c93df40a25709f8cf0f23f8800
Dan Fornika <dfornika@gmail.com>
2024-04-23 16:49:06 -0700

    Add some debugging statements

--- .github/scripts/push_container_images.sh
+++ .github/scripts/push_container_images.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-for image in ./apptainer-build/*.sif; do
-  IMAGE_NAME=$(basename $image .sif)
-  IMAGE_ID=ghcr.io/${{ github.repository_owner }}/$IMAGE_NAME
-  IMAGE_ID=$(echo $IMAGE_ID | tr '[A-Z]' '[a-z]')
-  VERSION=$(echo "${{ github.ref }}" | sed -e 's,.*/\(.*\),\1,')
-  [[ "${{ github.ref }}" == "refs/tags/"* ]] && VERSION=$(echo $VERSION | sed -e 's/^v//')
-  [ "$VERSION" == "main" ] && VERSION=latest
-  echo IMAGE_ID=$IMAGE_ID
-  echo VERSION=$VERSION
-  echo "Pushing $image to $IMAGE_ID:$VERSION..."
+for image_file in ./apptainer-build/*.sif; do
+    echo "Pushing image ${image_file}..."
+    IMAGE_NAME=$(basename ${image_file} .sif)
+    echo IMAGE_NAME=$IMAGE_NAME
+    IMAGE_ID=ghcr.io/${{ github.repository_owner }}/${IMAGE_NAME}
+    echo IMAGE_ID=$IMAGE_ID
+    IMAGE_ID=$(echo $IMAGE_ID | tr '[A-Z]' '[a-z]')
+    echo IMAGE_ID=$IMAGE_ID
+    VERSION=$(echo "${{ github.ref }}" | sed -e 's,.*/\(.*\),\1,')
+    [[ "${{ github.ref }}" == "refs/tags/"* ]] && VERSION=$(echo $VERSION | sed -e 's/^v//')
+    [ "$VERSION" == "main" ] && VERSION=latest
+    echo IMAGE_ID=$IMAGE_ID
+    echo VERSION=$VERSION
+    echo "Pushing $image to $IMAGE_ID:$VERSION..."
 done
