1a9a39db34679a059f9a0bf2062b71bbd8f43863
Dan Fornika <dfornika@gmail.com>
2024-04-24 11:01:42 -0700

    lowercase image ID

--- .github/scripts/push_container_images.sh
+++ .github/scripts/push_container_images.sh
@@ -5,11 +5,9 @@ for image_file in ./apptainer-build/*.sif; do
     IMAGE_NAME=$(basename ${image_file} .sif)
     echo IMAGE_NAME=$IMAGE_NAME
     IMAGE_ID=ghcr.io/${GITHUB_REPOSITORY_OWNER}/${IMAGE_NAME}
-    echo IMAGE_ID=$IMAGE_ID
+    IMAGE_ID=$(echo $IMAGE_ID | tr '[A-Z]' '[a-z]')
     VERSION=$(echo "${GITHUB_REF}" | sed -e 's,.*/\(.*\),\1,')
     [ "$VERSION" == "main" ] && VERSION=latest
-    echo IMAGE_ID=$IMAGE_ID
-    echo VERSION=$VERSION
     # Temporarily set version to test
     VERSION=test
     echo "Pushing $image to $IMAGE_ID:$VERSION..., using username ${GHCR_USERNAME}"
