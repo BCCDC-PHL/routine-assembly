6fcdf1e194543fe3f304fbce37d058d9d16167c4
Dan Fornika <dfornika@gmail.com>
2024-04-24 09:38:58 -0700

    Set secrets as env variables

--- .github/scripts/push_container_images.sh
+++ .github/scripts/push_container_images.sh
@@ -7,10 +7,11 @@ for image_file in ./apptainer-build/*.sif; do
     IMAGE_ID=ghcr.io/${GITHUB_REPOSITORY_OWNER}/${IMAGE_NAME}
     echo IMAGE_ID=$IMAGE_ID
     VERSION=$(echo "${GITHUB_REF}" | sed -e 's,.*/\(.*\),\1,')
-    [[ "${GITHUB_REF}" == "refs/tags/"* ]] && VERSION=$(echo $VERSION | sed -e 's/^v//')
     [ "$VERSION" == "main" ] && VERSION=latest
     echo IMAGE_ID=$IMAGE_ID
     echo VERSION=$VERSION
+    # Temporarily set version to test
+    VERSION=test
     echo "Pushing $image to $IMAGE_ID:$VERSION..."
-    echo ${{ secrets.GITHUB_TOKEN }} | apptainer registry login -u ${{ secrets.GHCR_USERNAME }} --password-stdin oras://ghcr.io
+    echo ${GITHUB_TOKEN} | apptainer registry login -u ${GHCR_USERNAME} --password-stdin oras://ghcr.io
 done
