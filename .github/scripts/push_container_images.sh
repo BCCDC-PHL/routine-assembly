739d4146f56d2eaa5928a213f30a3bff7efced48
Dan Fornika <dfornika@gmail.com>
2024-04-24 08:02:11 -0700

    Use env variable

--- .github/scripts/push_container_images.sh
+++ .github/scripts/push_container_images.sh
@@ -6,10 +6,8 @@ for image_file in ./apptainer-build/*.sif; do
     echo IMAGE_NAME=$IMAGE_NAME
     IMAGE_ID=ghcr.io/${GITHUB_REPOSITORY_OWNER}/${IMAGE_NAME}
     echo IMAGE_ID=$IMAGE_ID
-    IMAGE_ID=$(echo $IMAGE_ID | tr '[A-Z]' '[a-z]')
-    echo IMAGE_ID=$IMAGE_ID
-    VERSION=$(echo "${{ github.ref }}" | sed -e 's,.*/\(.*\),\1,')
-    [[ "${{ github.ref }}" == "refs/tags/"* ]] && VERSION=$(echo $VERSION | sed -e 's/^v//')
+    VERSION=$(echo "${GITHUB_REF}" | sed -e 's,.*/\(.*\),\1,')
+    [[ "${GITHUB_REF}" == "refs/tags/"* ]] && VERSION=$(echo $VERSION | sed -e 's/^v//')
     [ "$VERSION" == "main" ] && VERSION=latest
     echo IMAGE_ID=$IMAGE_ID
     echo VERSION=$VERSION
