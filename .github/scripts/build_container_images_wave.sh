7493965edd4a3edefd080f64459152a0287f5d84
Dan Fornika <dfornika@gmail.com>
2024-06-07 16:05:20 -0700

    Wave containers (#55)
    * Add wave install script
    
    * Build containers using wave
    
    * remove nonexistent --force flag
    
    * output json
    
    * update
    
    * update
    
    * updte
    
    * update

---
+++ .github/scripts/build_container_images_wave.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir -p artifacts
+
+for env_yaml in environments/*.yml; do
+    image_name=$(head -n 1 $env_yaml | cut -d ' ' -f 2)
+    echo "building image ${image_name} from file ${env_yaml}..."
+    wave \
+	--conda-file ${env_yaml} \
+	--singularity \
+	--freeze \
+	--await \
+	--output json \
+	| python -m json.tool \
+	| tee ${image_name}.json
+    echo "done building image ${image_name}"
+    cp ${image_name}.json artifacts/
+
+done
+
+
