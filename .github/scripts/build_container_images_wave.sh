f8f7bb4189f926f20571067ee2f03b728a3c50cf
Dan Fornika <dfornika@gmail.com>
2024-06-07 15:40:12 -0700

    update

--- .github/scripts/build_container_images_wave.sh
+++ .github/scripts/build_container_images_wave.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-mkdir -p apptainer-build
-pushd apptainer-build > /dev/null
+mkdir -p artifacts
 
 for env_yaml in ../environments/*.yml; do
     image_name=$(head -n 1 $env_yaml | cut -d ' ' -f 2)
     echo "building image ${image_name} from file ${env_yaml}..."
     wave \
+	--build-repo ghcr.io/bccdc-phl \
+	--cache-repo ghcr.io/bccdc-phl \
 	--conda-file ${env_yaml} \
 	--singularity \
 	--freeze \
 	--await \
 	--output json \
 	| python -m json.tool \
-		 | tee ${image_name}.json
-	echo "done building image ${image_name}"
+	| tee ${image_name}.json
+    echo "done building image ${image_name}"
+    cp ${image_name}.json artifacts/
+    
 done
 
 
