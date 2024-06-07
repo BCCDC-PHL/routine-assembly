5b623b2d818638ab288c43d8fbbe6bf0b5da2e4a
Dan Fornika <dfornika@gmail.com>
2024-06-07 15:47:34 -0700

    updte

--- .github/scripts/build_container_images_wave.sh
+++ .github/scripts/build_container_images_wave.sh
@@ -2,7 +2,7 @@
 
 mkdir -p artifacts
 
-for env_yaml in ../environments/*.yml; do
+for env_yaml in environments/*.yml; do
     image_name=$(head -n 1 $env_yaml | cut -d ' ' -f 2)
     echo "building image ${image_name} from file ${env_yaml}..."
     wave \
@@ -17,7 +17,7 @@ for env_yaml in ../environments/*.yml; do
 	| tee ${image_name}.json
     echo "done building image ${image_name}"
     cp ${image_name}.json artifacts/
-    
+
 done
 
 
