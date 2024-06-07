442d8540f27351dde58b72b4e50ebf641dcef4bc
Dan Fornika <dfornika@gmail.com>
2024-06-07 14:57:35 -0700

    Build containers using wave

---
+++ .github/scripts/build_container_images_wave.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p apptainer-build
+pushd apptainer-build > /dev/null
+
+for env_yaml in ../environments/*.yml; do
+    image_name=$(head -n 1 $env_yaml | cut -d ' ' -f 2)
+    echo "building image ${image_name} from file ${env_yaml}..."
+    wave \
+	--conda-file ${env_yaml} \
+	--singularity \
+	--freeze \
+	--force --await \
+	--output ${image_name}.sif
+
+    ls -lh ${image_name}.sif
+done
+
+
