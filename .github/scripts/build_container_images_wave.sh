c1405c7bdb6555c08ce9ec9cc169965ef18d0708
Dan Fornika <dfornika@gmail.com>
2024-06-07 15:52:04 -0700

    update

--- .github/scripts/build_container_images_wave.sh
+++ .github/scripts/build_container_images_wave.sh
@@ -6,8 +6,6 @@ for env_yaml in environments/*.yml; do
     image_name=$(head -n 1 $env_yaml | cut -d ' ' -f 2)
     echo "building image ${image_name} from file ${env_yaml}..."
     wave \
-	--build-repo ghcr.io/bccdc-phl \
-	--cache-repo ghcr.io/bccdc-phl \
 	--conda-file ${env_yaml} \
 	--singularity \
 	--freeze \
